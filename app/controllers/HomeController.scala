239cc482f95fd8b2e0e9e5c93ea56b20a57c42db
Fer_Carraro <carraro.fernando@gmail.com>
2020-12-20 13:21:01 -0600

    Se edito HomeController.scala

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,10 +4,7 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
