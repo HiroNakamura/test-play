1ca1d2392e4bd2a51ddd9a0351745b1bf5f0516a
Fer_Carraro <carraro.fernando@gmail.com>
2020-12-20 13:10:18 -0600

    Se ha creado el proyecto de Play Framework

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  //http://localhost:9000/
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+
+  //http://localhost:9000/saludo
+  def saludo() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.saludo())
+  }
+
+}
