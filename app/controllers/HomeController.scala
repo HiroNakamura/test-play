24fffddb73b42eb587b6b4d6860bd61db3f39754
Fer_Carraro <carraro.fernando@gmail.com>
2021-02-01 13:48:45 -0600

    Se han agregado nuevas clases: Cliente.scala y se edito configuracion

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,8 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.db._
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
---
+++ app/models/Cliente.scala
@@ -0,0 +1,11 @@
+package models
+
+
+val connection = DB.getConnection("mariadb")
+
+case class Cliente (
+    id: Int,
+    nombre: String, 
+    apellido:String, 
+    servicio_id:String
+)
